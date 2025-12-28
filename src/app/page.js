3896b2a5ad30a668d9bd3a77a6c5f2244d9897a2
Narendra Mishra <narendramishra@mac.home>
2025-12-27 19:06:10 -0500

    refactor: globalize Footer and Navbar by moving to RootLayout

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Navbar from "@/components/Navbar";
 import Hero from '../components/Hero';
 import FindMySign from '@/components/FindMySign';
 import ZodiacGrid from '../components/ZodiacGrid';
 import TarotReading from '@/components/TarotReading';
 import About from '@/components/About';
-import Footer from '../components/Footer';
 import Panchang from '@/components/Panchang';
 import LoadingScreen from '@/components/LoadingScreen'; // Import Loader
 
@@ -12,14 +10,12 @@ export default function Home() {
   return (
     <main>
       <LoadingScreen /> {/* Added Sri Yantra Loader */}
-      <Navbar />
       <Hero />
       <Panchang />
       <FindMySign />
       <ZodiacGrid />
       <TarotReading />
       <About />
-      <Footer />
     </main>
   );
 }
